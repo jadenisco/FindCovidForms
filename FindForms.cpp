677eee84e24ba94588144427b95e1d6567c1d7b8
John DeNisco <jadenisco17@gmail.com>
2022-09-28 07:21:19 -0400

    Initial Commit

---
+++ FindForms.cpp
@@ -0,0 +1,19 @@
+//
+//  FindForms.cpp
+//
+//  Created by John DeNisco on 2/5/22.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int x;
+
+    cout << "Hello World!\n";
+//    cout << "Enter a Number: ";
+//    cin >> x;
+//    cout << "The number is: " << x;
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# FindForms
+
+This program will Find Filled out Covid Forms. We will ask form the Volunteer number and the date the form should have been filled out.
