b740953184a5eb87052722210b58fb13bad49197
John DeNisco <jadenisco17@gmail.com>
2022-10-01 07:50:24 -0400

    Handle the date

--- FindForms.cpp
+++ FindForms.cpp
@@ -8,10 +8,13 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <iomanip>
+#include <stdio.h>
 
 using namespace std;
 
-// Local Definitions
+// Local Definitions, these should go in a header file
 //#define MGH
 //#define CONSOLE_INPUT
 
@@ -32,26 +35,70 @@ struct FormInfo {
     string dateAsString;
 };
 
+// Test Values
 static FormInfo formInfo[] = {{"450", "12345678"},
-                            {"350", "11/18/2021"},
+                            {"350", "11/35/2021"},
+                            {"345", "14/18/2021"},
+                            {"100085", "06/00/2021"},
                             {"123", "04/11"},
                             {"0725", "6/11/2022"},
                             {"342", "1/9/2022"}
                             };
 static int formInfoIndex = 0;
 
+// Takes a pointer to a date, the date obtained will be returned in this structure.
+// returns false, if the parse failed
+static bool GetDate(tm *date) {
+    string dateAsString = formInfo[formInfoIndex++].dateAsString;
+
+    cout << "Date as String: " << dateAsString << "\n";
+    istringstream ss(dateAsString);
+    ss >> get_time(date, "%D");
+
+    if (ss.fail()) {
+        std::cout << "The date is not valid, Please enter a valid date.\n";
+        return(false);
+    }
+    else {
+        std::cout << put_time(date, "Parsed Date: %D") << '\n';
+        return(true);
+    }
+}
+
+static void GetInfo() {
+    char sbuf[64];
+    string month;
+    string day;
+    
+    for(formInfoIndex=0; formInfoIndex < sizeof(formInfo)/sizeof(FormInfo);) {
+        tm date = {};
+
+        if(GetDate(&date)) {
+            strftime(sbuf, sizeof(sbuf), "%b", &date);
+            string month(sbuf);
+            cout << "MONTH: " << month << "\n";
+            strftime(sbuf, sizeof(sbuf), "%d", &date);
+            string day(sbuf);
+            cout << "DAY: " << day << "\n";
+            strftime(sbuf, sizeof(sbuf), "%Y", &date);
+            string year(sbuf);
+            cout << "YEAR: " << year << "\n";
+        }
+    }
+}
+
 int main(int argc, const char * argv[]) {
     int x;
 
-    cout << "Hello World!\n";
-    cout << "Enter a Number: ";
+    GetInfo();
 
+    cout << "Enter a Number: ";
 #ifdef CONSOLE_INPUT
     cin >> x;
 #else
     x = 10;
 #endif
 
-    cout << "The number is: " << x;
+    cout << "The number is: " << x << "\n";
     return 0;
 }
