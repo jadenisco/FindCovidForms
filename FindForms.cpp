90044870194da0d4583b1a730373530a837725f7
John DeNisco <jadenisco17@gmail.com>
2022-10-05 09:02:01 -0400

    Finish find form

--- FindForms.cpp
+++ FindForms.cpp
@@ -10,17 +10,18 @@
 #include <sstream>
 #include <iomanip>
 #include <filesystem>
+#include <regex>
 
 namespace fs = std::filesystem;
 using namespace std;
 
 // Local Definitions, these should go in a header file
-//#define MGH
-//#define CONSOLE_INPUT
+#define MGH
+#define CONSOLE_INPUT
 
-#if MGH
-static String vol_root_dir = "//Cifs2/voldept$";
-static String? pet_volunteer_dir = vol_root_dir + "/.Volunteer Files/Pet Therapy";
+#ifdef MGH
+static string vol_root_dir = "//Cifs2/voldept$";
+static string pet_volunteer_root_dir = vol_root_dir + "/.Volunteer Files/Pet Therapy";
 #else
 static string vol_root_dir = "/Users/jdenisco/Developer/Windows/testroot";
 static string pet_volunteer_root_dir;
@@ -43,16 +44,17 @@ typedef struct VolMonDayYear {
 } VolMonDayYearDef;
 
 // Test Values
-static FormInfo formInfo[] = {{"350", "6/10/21"}};
-////                            {"450", "12345678"},
-////                            {"john", "11/1/2021"},
-////                            {"350", "11/35/2021"},
-////                            {"345", "14/18/2021"},
-////                            {"100085", "06/00/2021"},
-////                            {"123", "04/11"},
-////                            {"0725", "6/11/2022"},
-////                            {"342", "1/9/2022"}
-////                            };
+static FormInfo formInfo[] = {{"350", "6/10/21"},
+                                {"350", "6/1/2021"},
+                                {"450", "12345678"},
+                                {"john", "11/1/2021"},
+                                {"350", "11/35/2021"},
+                                {"345", "14/18/2021"},
+                                {"100085", "06/00/2021"},
+                                {"123", "04/11"},
+                                {"0725", "6/11/2022"},
+                                {"342", "1/9/2022"}
+                                };
 static int formInfoIndex = 0;
 
 // GetVol
@@ -74,14 +76,14 @@ static int GetVol(VolMonDayYear *volMonDayYear) {
 #else
         volAsString = formInfo[formInfoIndex].volunteer_number;
 #endif
-        cout << "Vol as String: " << volAsString << "\n";
+//        cout << "Vol as String: " << volAsString << "\n";
 
         try {
             volNum = stoi(volAsString);
         }
         catch(exception &err) {
-            cout << err.what() << "\n";
-            cout << "The volunteer number '" << volAsString << "' is not valid, Please enter a valid number.\n";
+//            cout << err.what() << "\n";
+//            cout << "The volunteer number '" << volAsString << "' is not valid, Please enter a valid number.\n";
 
             formInfoIndex++;
             continue;
@@ -111,16 +113,16 @@ static bool GetDate(tm *date) {
         dateAsString = formInfo[formInfoIndex].dateAsString;
 #endif
 
-        cout << "Date as String: " << dateAsString << "\n";
+        cout << "Date: " << dateAsString << "\n";
         istringstream ss(dateAsString);
         ss >> get_time(date, "%D");
 
         if (ss.fail()) {
-            cout << "The date " << dateAsString << " is not valid, Please enter a valid date.\n";
+//            cout << "The date " << dateAsString << " is not valid, Please enter a valid date.\n";
             formInfoIndex++;
         }
         else {
-            cout << put_time(date, "Parsed Date: %D") << '\n';
+//            cout << put_time(date, "Parsed Date: %D") << '\n';
             return(true);
         }
     }
@@ -135,32 +137,56 @@ static void GetInfo(VolMonDayYear *volMonDayYear) {
         strftime(sbuf, sizeof(sbuf), "%b", &date);
         volMonDayYear->month = string(sbuf);
         volMonDayYear->month[0] = tolower(volMonDayYear->month[0]);
-        cout << "MONTH: " << volMonDayYear->month << "\n";
+//        cout << "MONTH: " << volMonDayYear->month << "\n";
         strftime(sbuf, sizeof(sbuf), "%d", &date);
         volMonDayYear->day = string(sbuf);
-        cout << "DAY: " << volMonDayYear->day << "\n";
+//        cout << "DAY: " << volMonDayYear->day << "\n";
         strftime(sbuf, sizeof(volMonDayYear->year), "%Y", &date);
         volMonDayYear->year = string(sbuf);
-        cout << "YEAR: " << volMonDayYear->year << "\n";
+//        cout << "YEAR: " << volMonDayYear->year << "\n";
     }
 
     if((volNum = GetVol(volMonDayYear)) != 0) {
-        cout << "VOL NUM: " << volMonDayYear->volNum << "\n";
+//        cout << "VOL NUM: " << volMonDayYear->volNum << "\n";
     }
 }
 
-static void ParseDir(const fs::path& pathToScan, int level = 0) {
+static void ParseDir(const fs::path& pathToScan, VolMonDayYear *volMonDayYear, int level = 0) {
+    char sbuf[42];
+    string str;
+    smatch m;
+
+    //Should be done once
+    snprintf(sbuf, sizeof(sbuf), "%s (\\w+)", volMonDayYear->volNum.c_str());
+    str = string(sbuf);
+    regex ve(str);
+    snprintf(sbuf, sizeof(sbuf), "Covid.*%s", volMonDayYear->year.c_str());
+    str = string(sbuf);
+    regex cf(str, regex_constants::ECMAScript | regex_constants::icase);
+    snprintf(sbuf, sizeof(sbuf), "%s", volMonDayYear->month.c_str());
+    str = string(sbuf);
+    regex me(str, regex_constants::ECMAScript | regex_constants::icase);
+
+    if(pathToScan.empty()) {
+        return;
+    }
 
     for(const auto& entry : fs::directory_iterator(pathToScan)) {
-        cout << "L: " << level << setw(1) << "";
         const auto filenameStr = entry.path().filename().string();
-        if (entry.is_directory()) {
-            cout << setw(level * 3) << "" << "D: " << filenameStr << "\n";
-            ParseDir(entry, level +1 );
-        } else if ( entry.is_regular_file()) {
-            cout << setw(level * 3) << "" << "F: " << filenameStr << "\n";
+
+        if(level == 0 && entry.is_directory() && regex_search(filenameStr, m, ve)) {
+            cout << "\n" << filenameStr << "\n";
+            ParseDir(entry, volMonDayYear, level + 1);
+        } else if(entry.is_directory() && regex_search(filenameStr, m, cf)) {
+            cout << setw(level * 2) << "" << filenameStr << "\n";
+            ParseDir(entry, volMonDayYear, level + 1);
+        } else if (entry.is_directory() && regex_search(filenameStr, m, me)) {
+            cout << setw(level * 2) << "" << filenameStr << "\n";
+            ParseDir(entry, volMonDayYear, level + 1);
+        } else if (level > 0) {
+            cout << setw(level * 2) << "" << filenameStr << "\n";
         } else {
-            cout << setw(level * 3) << "" << " [?]" << filenameStr << "\n"; 
+            cout << ".";
         }
     }
 } 
@@ -175,7 +201,10 @@ int main(int argc, const char * argv[]) {
         VolMonDayYear volMonDayYear;
 
         GetInfo(&volMonDayYear);
-        ParseDir(adult_volunteer_root_dir);
+        ParseDir(adult_volunteer_root_dir, &volMonDayYear);
+        ParseDir(junior_volunteer_root_dir, &volMonDayYear);
+        ParseDir(pet_volunteer_root_dir, &volMonDayYear);
+        cout << "\n";
     }
 
     return 0;
